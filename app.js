cc2e22ff14c1d635fa3e77379bfcbdbf505bf8ff
Yuka <yukamoribe@outlook.com>
2020-12-01 22:35:17 -0500

    Add code to initialize db.

--- app.js
+++ app.js
@@ -1,5 +1,56 @@
+const cors = require('cors');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser')
 const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const logger = require('morgan');
+const csurf = require('csurf');
+const routes = require('./routes');
 
 const app = express();
 
-export default app;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser())
+
+app.use(cors({ origin: true }));
+app.use(helmet({ hsts: false }));
+app.use(csurf({
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production',
+    httpOnly: true
+  }
+}));
+
+
+app.use(routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get(/\/(?!api)*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
+app.use(function(_req, _res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, _req, res, _next) {
+  res.status(err.status || 500);
+  if (err.status === 401) {
+    res.set('WWW-Authenticate', 'Bearer');
+  }
+  res.json({
+    message: err.message,
+    error: JSON.parse(JSON.stringify(err)),
+  });
+});
+
+module.exports = app;
---
+++ documentation/MVP.md
@@ -0,0 +1,18 @@
+# MVP
+
+## User
+* Ability to create heroes and stories.
+
+## Hero
+* Ability to grow stats and levels depending on daily accomplishments.
+* Ability to save daily journals.
+
+## Story
+* Belongs to a world.
+* Ability to create destinations(goals).
+* Ability to create monsters(constraints).
+* Ability to keep track of how many days the hero has left before it hits the target date set on final destination(final goal).
+
+## Stretch Goals
+* Have the hero be able to hold skills and classes.
+* Create a hub for heroes that belong to the same world.
