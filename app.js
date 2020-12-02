ee5d00f170bfcf261015212e006b6d4e9ff317c3
Yuka <yukamoribe@outlook.com>
2020-12-01 23:02:04 -0500

    Test and fix code so db is actually created.

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(csurf({
 }));
 
 
-app.use(routes);
+// app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
--- config/database.js
+++ config/database.js
@@ -1,23 +1,18 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+const config = require('./');
+const { username, password, database, host } = config.db;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: 'sequelize'
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
   }
 }
