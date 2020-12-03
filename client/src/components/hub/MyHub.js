9e4b47e6e841175dbfd611715eeb600f9a2c0633
Yuka <yukamoribe@outlook.com>
2020-12-03 15:53:04 -0500

    Build basic grid for myhub page.

--- client/src/components/hub/MyHub.js
+++ client/src/components/hub/MyHub.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 const MyHub = () => {
   return ( 
-    <>
-      'MyHub'
-    </>
+    <div className="hub">
+      <header className='hub__header'>
+        <nav className="hub__navbar"></nav>
+      </header>
+      <main className="hub__main">
+        <div className="hub__activity">
+          Activity bar graph
+        </div>
+      </main>
+
+      <aside className="hub__sidebar">
+
+      </aside>
+    </div>
   );
 }
  
