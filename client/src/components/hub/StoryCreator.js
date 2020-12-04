2a96d72ae50cb2bd178f01ab9eb244661f719e41
Yuka <yukamoribe@outlook.com>
2020-12-04 15:58:13 -0500

    Implement functionality to toggle world selection and styling to currently-selected world in modal.

--- client/src/components/hub/StoryCreator.js
+++ client/src/components/hub/StoryCreator.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {baseUrl} from '../../config';
 
 const StoryCreator = () => {
+  const container = useRef();
+
   const [page, setPage] = useState(1);
   const [worlds, setWorlds] = useState([]);
   const [world, setWorld] = useState(null);
@@ -13,26 +15,46 @@ const StoryCreator = () => {
       if (!worlds.length) {
         const res = await fetch(`${baseUrl}/worlds`);
         const data = await res.json();
-        console.log(data.worlds)
-        setWorlds(data.worlds)
-        // await setWorlds([1, 2, 3])
-        // console.log(worlds)
-        console.log(worlds, 2)
-
+        setWorlds(data.worlds);
       }
     })();
   }, [worlds]);
 
+  useEffect(() => {
+    console.log(world, 'world')
+    if (container.current.children.length) {
+      container.current.childNodes.forEach(child => child.classList.remove('selected'));
+      container.current.childNodes[world - 1].classList.add('selected');
+    }
+  }, [world])
+
+  const handleSelect = e => {
+    e.target.classList.add('selected')
+    console.log(e.target.classList)
+  }
+
+  const handleBack = () => {
+    setPage(page - 1);
+  }
+
+  const handleNext = () => {
+    setPage(page + 1);
+  }
+
   return ( 
     <>
       <h2>Select Your World</h2>
-      <div className="world__container"> 
-        {worlds.length && worlds.map(world => (
-          <div className="world">
-            <div className="world__name">{world.name}</div>
-          </div>
-        ))}
-      </div>
+      {page === 1 && (
+        <div className="world__container" ref={container}> 
+          {worlds.length && worlds.map(world => (
+            <div className="world" onClick={() => setWorld(world.id)}>
+              <div className="world__name">{world.name}</div>
+            </div>
+          ))}
+        </div>
+      )}
+      <button disabled={page < 2} onClick={handleBack}>Back</button>
+      <button disabled={page > 2} onClick={handleNext}>Next</button>
     </>
   );
 }
