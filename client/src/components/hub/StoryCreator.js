b84c51267dbf5bec7273eb48b7938c215edb9a45
Yuka <yukamoribe@outlook.com>
2020-12-04 16:53:49 -0500

    Style to display currently-selected hero in modal.

--- client/src/components/hub/StoryCreator.js
+++ client/src/components/hub/StoryCreator.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import {baseUrl} from '../../config';
 
 const StoryCreator = () => {
-  const container = useRef();
+  const worldContainer = useRef();
+  const heroContainer = useRef();
 
   const [page, setPage] = useState(1);
   const [worlds, setWorlds] = useState([]);
@@ -27,26 +28,30 @@ const StoryCreator = () => {
       if (!heroes.length) {
         const res = await fetch(`${baseUrl}/heroes`);
         const data = await res.json();
-        console.log(data, '!!!!')
         setHeroes(data.heroImages);
       }
     })();
   }, [heroes])
 
   useEffect(() => {
-    if (container.current.children.length) {
-      container.current.childNodes.forEach(child => child.classList.remove('selected'));
-      container.current.childNodes[world - 1].classList.add('selected');
-    }
-  }, [world])
-
-  useEffect(() => {
     if (page === 1) {
       setPageTitle('Select Your World');
-    } else if (page === 2) {
-      setPageTitle('Select Your Hero');
+      if (worldContainer.current.children.length && world) {
+        worldContainer.current.childNodes.forEach(child => child.classList.remove('selected'));
+        worldContainer.current.childNodes[world - 1].classList.add('selected');
+      }
     }
-  }, [page])
+  }, [world, page])
+
+  useEffect(() => {
+    if (page === 2) {
+      setPageTitle('Select Your Hero');
+      if (heroContainer.current.children.length && hero) {
+        heroContainer.current.childNodes.forEach(child => child.classList.remove('selected'));
+        heroContainer.current.childNodes[hero - 1].classList.add('selected');
+      }
+    }
+  }, [hero, page])
 
   const handleBack = () => {
     setPage(page - 1);
@@ -60,7 +65,7 @@ const StoryCreator = () => {
     <>
       <h2>{pageTitle}</h2>
       {page === 1 && (
-        <div className="modal__page-container" ref={container}> 
+        <div className="modal__page-container" ref={worldContainer}> 
           {worlds.length && worlds.map(world => (
             <div className="world" onClick={() => setWorld(world.id)}>
               <div className="world__name">{world.name}</div>
@@ -70,7 +75,7 @@ const StoryCreator = () => {
       )}
       {page === 2 && (
         <div>
-          <div className="modal__page-container" ref={container}> 
+          <div className="modal__page-container" ref={heroContainer}> 
             {heroes.length && heroes.map(hero => (
               <div className="hero" onClick={() => setHero(hero.id)}>
                 <img src={hero.image_url} alt={hero.id} />
