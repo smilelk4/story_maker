02edefb3daff52032ee97de50bde532d0acb2179
Yuka <yukamoribe@outlook.com>
2020-12-07 21:57:38 -0500

    Two lines showing up in line chart but still working on rendering them dynamically.

--- client/src/components/hub/Activity.js
+++ client/src/components/hub/Activity.js
@@ -1,66 +1,74 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { select, line, curveCardinal, axisBottom, scaleLinear } from 'd3';
+import {nest} from 'd3-collection';
 
 const Activity = ({activities}) => {
   const [points, setPoints] = useState({});
   const chart = useRef();
 
+  // const points = [];
+
   useEffect(() => {
-    console.log('CONTAINER')
-    console.log(activities)
 
-    // if (!points.length && activities) {
-    //   for (let heroId in activities) {
-        // if (!(heroId in points)) {
-        //   setPoints(points[heroId] = activities[heroId].map(activity => (
-        //     activity.point
-        //   )));
-        // }
-      // }
+    if (Object.keys(points).length !== Object.keys(activities).length) {
 
-    //  setPoints(activities.map(activity => activity.point));
-    // }
-
-    const svg = select(chart.current);
-    const xScale = scaleLinear().domain([0, 31])
-                                .range([0, 100]);
-
-    const yScale = scaleLinear().domain([0, 10])
-                                .range([150, 0])
-
-    // const xAxis = axisBottom(xScale);
-
+      const xScale = scaleLinear().domain([0, 31])
+                                  .range([0, 100]);
     
-    const colors = ['red', 'blue', 'green'];
-    
-    if (Object.keys(activities).length) {
-      for (let heroData in activities) {
-        for (let monthData in activities[heroData]) {
-          let dataLine = line().x((value, index) => xScale(index))
-                                  .y(yScale)
-                                  .curve(curveCardinal);
-          console.log(activities[heroData][monthData])
-          svg.selectAll('path').data([activities[heroData][monthData]]).join('path')
-                      .attr('d', dataLine)
-                      .attr('fill', 'none')
-                      .attr('stroke', colors[Math.floor(Math.random() * 3)]);
+      const yScale = scaleLinear().domain([0, 10])
+                                  .range([150, 0]);
+  
+      let dataLine = line().x((value, index) => xScale(index))
+                            .y(yScale)
+                            .curve(curveCardinal)
+
+      for (let heroId in activities) {
+        let combinedMonthData = [];
+        for (let monthData in activities[heroId]) {
+          combinedMonthData.push(...activities[heroId][monthData]);
         }
+
+        // const svg = select(chart.current);
+        // svg.selectAll("path")
+        //   .data(combinedMonthData)
+        //   .enter()
+        //   .append("path")
+        //     .attr("fill", "none")
+        //     .attr('stroke', 'red')
+        //     .attr("stroke-width", 1.5)
+        //     .attr("d", function(d){
+        //       return line()
+        //       .x((value, index) => xScale(index))
+        //       .y(yScale)
+        //       .curve(curveCardinal)
+        //     })
+
+        
+
+        
+        setPoints({
+          ...points,
+          [heroId]: dataLine(combinedMonthData)
+        });
       }
-    }
+
+      // const svg = select(chart.current);
+  
+      // const a = dataLine(activities['1']['12']);
+      // const b = dataLine([4, 5, 4]);
     
-    // svg.selectAll('path').data([points]).join('path')
-    //                       .attr('d', dataLine)
-    //                       .attr('fill', 'none')
-    //                       .attr('stroke', 'blue');
+      // setPoints({ a, b });
+    }
+
   }, [activities, points]);
 
   return ( 
     <div className="activity">
       <svg ref={chart}>
-        {/* <path d="M10,150 100,100 150,120" stroke="blue" fill="none" /> */}
+           <path d={points['1']} fill='none' stroke={'red'} />
+           <path d={points['2']} fill='none' stroke={'blue'} />
       </svg>
     </div>
   );
 }
- 
 export default Activity;
