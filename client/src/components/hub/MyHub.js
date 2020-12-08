078be84d3dd64c2c0808070434abf33d7309f390
Yuka <yukamoribe@outlook.com>
2020-12-08 15:45:10 -0500

    Restructured css to have background color on hub page.

--- client/src/components/hub/MyHub.js
+++ client/src/components/hub/MyHub.js
@@ -12,32 +12,34 @@ import Scroll2 from '../svg/Scroll2';
 const MyHub = () => {
   return ( 
     <div className="hub">
-      <main className="hub__main">
-        <div className="hub__activity left-section">
-          <ActivityScroll />
-          <ActivityContainer />
-        </div>
-        <div className="hub__destination-container left-section">
-          <DestinationScroll />
-          <DestinationContainer />
-        </div>
-      </main>
-
-      <aside className="hub__sidebar">
-        <div className="hub__section">
-          <div className="hub__icon-holder">
-            <NewAdventure />
+      <div className="hub__contents">
+        <main className="hub__main">
+          <div className="hub__activity left-section">
+            <ActivityScroll />
+            <ActivityContainer />
           </div>
-        </div>
-        <div className="hub__section">
-          <Scroll2 text="Your Heroes"/>
-          <HeroContainer />
-        </div>
-        <div className="hub__section">
-          <Scroll2 text="Stories"/>
-          <StoryContainer />
-        </div>
-      </aside>
+          <div className="hub__destination-container left-section">
+            <DestinationScroll />
+            <DestinationContainer />
+          </div>
+        </main>
+
+        <aside className="hub__sidebar">
+          <div className="hub__section">
+            <div className="hub__icon-holder">
+              <NewAdventure />
+            </div>
+          </div>
+          <div className="hub__section">
+            <Scroll2 text="Your Heroes"/>
+            <HeroContainer />
+          </div>
+          <div className="hub__section">
+            <Scroll2 text="Stories"/>
+            <StoryContainer />
+          </div>
+        </aside>
+      </div>
     </div>
   );
 }
