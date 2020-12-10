c2dbcec56777a515f632860af3124abf330e5b4d
Yuka <yukamoribe@outlook.com>
2020-12-10 09:20:45 -0500

    Add some default seed data for daily tasks.

---
+++ db/seeders/20201210141135-default-daily-tasks.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('DailyTasks', [
+      {
+        hero_id: 1,
+        title: 'Read a programming-related article',
+        last_accomplished: ( d => new Date(d.setDate(d.getDate()-1)) )(new Date),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        hero_id: 1,
+        title: 'Solve a leet code problem',
+        last_accomplished: ( d => new Date(d.setDate(d.getDate()-1)) )(new Date),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        hero_id: 1,
+        title: 'Work for an hour on a React project',
+        last_accomplished: ( d => new Date(d.setDate(d.getDate()-1)) )(new Date),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('DailyTasks', null, {});
+  }
+};
