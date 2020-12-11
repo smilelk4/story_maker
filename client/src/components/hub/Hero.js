006e42c6d6b5db7512db8364045a2a9e34e61e80
Yuka <yukamoribe@outlook.com>
2020-12-11 18:19:03 -0500

    Add HP and XP bars on hub page.

--- client/src/components/hub/Hero.js
+++ client/src/components/hub/Hero.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Hero = (...props) => {
-  const { id, name, level, hp, image } = props[0];
+  const { id, name, level, hp, xp, image } = props[0];
+  const maxHP = 100;
+  const maxXP = Math.floor(100 ** (level / 50) * 10);
+  const [hpPercentage, setHPPercentage] = useState("100%");
+  const [xpPercentage, setXPPercentage] = useState("100%");
+
+  useEffect(() => {
+    setHPPercentage(`${(hp / maxHP) * 100}%`);
+    setXPPercentage(`${(xp / maxXP) * 100}%`);
+  }, [hp, xp]);
+
   return ( 
     <div className="hero">
       <div className="hero__avatar">
         <img src={image} alt={id} />
       </div>
       <p className="hero__stat">
-          <span>{level}</span><span>{hp}</span>
+          <span className="hero__level">{level}</span>
+          <p className="hero__status-bars-container">
+            <span className="status-bar">
+              <span 
+              style={{width: hpPercentage }}
+              className="hp-meter"></span>
+            </span>
+            <span className="status-bar">
+              <span 
+              style={{width: xpPercentage }}
+              className="xp-meter"></span>
+            </span>
+          </p>
       </p>
       <p className="hero__name">{name}</p>
     </div>
