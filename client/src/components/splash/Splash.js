d0ae77fc1ef895dc3287aeecdd5c795ebff5d1f5
Yuka <yukamoribe@outlook.com>
2020-12-11 11:57:12 -0500

    Add some contents to splash page.

--- client/src/components/splash/Splash.js
+++ client/src/components/splash/Splash.js
@@ -3,7 +3,7 @@ import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
 
 const Splash = () => {
-  const [form, setForm] = useState(<SignupForm />);
+  const [form, setForm] = useState(<LoginForm />);
 
   const changeForm = (component) => {
     setForm(component);
@@ -11,6 +11,19 @@ const Splash = () => {
 
   return ( 
     <div className="splash">
+      <main className="splash__main">
+        <section className="splash__section">
+          <h1 className="splash__title title">Story Maker</h1>
+        </section>
+        <section className="splash__description">
+          <p>Create your own story.</p>
+          <p>Build habits.</p>
+          <p>Achieve your goals.</p>
+        </section>
+        <footer className="splash__footer">
+          Developed by Yuka Moribe
+        </footer>
+      </main>
       <div className="splash__container">
         <div className="splash__scroll">
           <p className="splash__toggle-tag">
