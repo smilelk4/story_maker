904e66512ee9aa479c34755d29e3c35d3ebf3831
Yuka <yukamoribe@outlook.com>
2020-12-11 13:39:48 -0500

    Add logic to display days left until desired destination.

--- client/src/components/Destination.js
+++ client/src/components/Destination.js
@@ -4,11 +4,17 @@ import dateFormatter from '../utils/dateFormatter';
 const Destination = ({...props}) => {
   const { id, parent_destination_id: parentDestinationId,
         title, description, target_date: targetDate, importance } = props;
+
+  const today = new Date();
+  const target = new Date(targetDate);
+  const timeDiff = target.getTime() - today.getTime();
+  const dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
   return ( 
     <div className="destination">
       <p className="destination__title title">{title}</p>
       <p className="destination__description">{description}</p>
-      <p className="destination__days-left">__ Days Left</p>
+      <p className="destination__days-left">{dayDiff > 0 ? `${dayDiff} Days Left`: 'Overdue'} </p>
       <p className="destination__target-date">Target Date: {dateFormatter(targetDate)}</p>
     </div>
   );
