7252651538fbfd4da7562343c754065f0862de46
Yuka <yukamoribe@outlook.com>
2020-12-12 22:50:00 -0500

    Implement horizontal scrolling to heroes container.

--- client/src/components/hub/HeroContainer.js
+++ client/src/components/hub/HeroContainer.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
 import Hero from './Hero';
 import { getHeroes } from '../../store/actions/heroAction';
 import NewHeroModal from './NewHeroModal';
@@ -9,6 +10,17 @@ const HeroContainer = () => {
   const userId = useSelector(state => state.user.id);
   const heroes = useSelector(state => state.hero);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const container = useRef(null);
+  const arrowRight = useRef();
+  const arrowLeft = useRef();
+
+  const handleLeftScroll = () => {
+    container.current.scrollLeft -= 100;
+
+  };
+  const handleRightScroll = () => {
+    container.current.scrollLeft += 100;
+  };
 
   useEffect(() => {
     if (userId) {
@@ -21,7 +33,11 @@ const HeroContainer = () => {
   };
 
   return (
-    <div className="hero__container">
+    <div className="hero__container" ref={container}>
+        <div className="hero__container-left-scroll" ref={arrowLeft} 
+             onClick={handleLeftScroll}>&#9001;</div>
+        <div className="hero__container-right-scroll" ref={arrowRight} 
+             onClick={handleRightScroll}>&#9002;</div>
       {isModalOpen && <NewHeroModal clickHandler={clickHandler} />}
       <h4 
         className="hero__new-hero"
