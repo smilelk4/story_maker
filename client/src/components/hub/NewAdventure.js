c3741a30a455b8bc1c590e77f4b54aa92436583c
Yuka <yukamoribe@outlook.com>
2020-12-16 18:42:15 -0500

    Create sword animation and add to main page.

--- client/src/components/hub/NewAdventure.js
+++ client/src/components/hub/NewAdventure.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 import NewStoryModal from './NewStoryModal';
+import bodymovin from 'lottie-web';
 
 const NewAdventure = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const swordsAnimation = bodymovin.loadAnimation({
+    wrapper: document.querySelector('.new-adventure__svg-container'),
+    animType: 'svg',
+    loop: false,
+    path: '/svg/swords.json',
+  });
 
   const clickHandler = () => {
     setIsModalOpen(!isModalOpen);
@@ -13,7 +20,10 @@ const NewAdventure = () => {
     {isModalOpen && <NewStoryModal clickHandler={clickHandler} />}
     <h3 
       className="hub__new-adventure"
-      onClick={clickHandler}>Start a New Adventure</h3>
+      onClick={clickHandler}>
+      <p 
+        className="new-adventure__svg-container"></p>
+      Start a New Adventure</h3>
     </>
   );
 }
