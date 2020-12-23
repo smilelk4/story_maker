569551eb647ebd685ecc038499477c1a88a84d50
Yuka <yukamoribe@outlook.com>
2020-12-23 16:53:10 -0500

    Add some new images to MonsterImage seed data and modify default monster seed to include some more data.

--- db/seeders/20201218154454-default-monster-images.js
+++ db/seeders/20201218154454-default-monster-images.js
@@ -4,12 +4,12 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('MonsterImages', [
       {
-        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/goblin.png",
+        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/bat.png",
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/mage-goblin.png",
+        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/goblin.png",
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -19,12 +19,27 @@ module.exports = {
         updatedAt: new Date()
       },
       {
+        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/mage-goblin.png",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
         image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/slime.png",
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/bat.png",
+        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/golem.png",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/mage.png",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        image_url: "https://story-maker-app.s3.amazonaws.com/monster-icons/knight.png",
         createdAt: new Date(),
         updatedAt: new Date()
       },
--- db/seeders/20201218154456-default-monsters.js
+++ db/seeders/20201218154456-default-monsters.js
@@ -7,20 +7,35 @@ module.exports = {
         story_id: 1,
         name: 'Procastination',
         strength: 4,
+        image_id: 4,
+        times_defeated: 4,
         createdAt: new Date(2020, 11, 5),
         updatedAt: new Date(2020, 11, 5)
       },
       {
         story_id: 1,
-        name: 'Video game',
+        name: 'Video Game',
         strength: 6,
+        image_id: 6,
+        times_defeated: 11,
         createdAt: new Date(2020, 11, 6),
         updatedAt: new Date(2020, 11, 6)
       },
       {
         story_id: 1,
-        name: 'Netflix',
+        name: 'Netflix Binge Watch',
         strength: 3,
+        image_id: 3,
+        times_defeated: 9,
+        createdAt: new Date(2020, 11, 5),
+        updatedAt: new Date(2020, 11, 5)
+      },
+      {
+        story_id: 1,
+        name: 'Net Surfing',
+        strength: 5,
+        image_id: 5,
+        times_defeated: 2,
         createdAt: new Date(2020, 11, 5),
         updatedAt: new Date(2020, 11, 5)
       },
