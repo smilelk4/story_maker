aaac05acc5b823d9120f129ded8268720cc154cd
Yuka <yukamoribe@outlook.com>
2021-01-19 19:59:31 -0500

    Add some seed data for destinations on story2.

--- db/seeders/20201205163116-default-destinations.js
+++ db/seeders/20201205163116-default-destinations.js
@@ -229,6 +229,116 @@ module.exports = {
         updatedAt: new Date(2020, 12, 10)
       },
       {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Get a certification",
+        description: ``,
+        target_date: new Date(2021, 3, 1),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Attend a live lecture",
+        description: `Checkup meetups`,
+        target_date: new Date(2021, 4, 3),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Learn about Stripe",
+        description: `A technology to receive transactions`,
+        target_date: new Date(2021, 5, 4),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Read The Go-Giver",
+        description: `I heard it's a good book to read about business`,
+        target_date: new Date(2021, 6, 20),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Learn about security",
+        description: `Which technology should I implement to protect client info?`,
+        target_date: new Date(2021, 6, 30),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Learn about SEO",
+        description: `Maybe take some courses on Coursera`,
+        target_date: new Date(2021, 7, 7),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Find a mentor",
+        description: `I need someone to advise and watch over me`,
+        target_date: new Date(2021, 8, 19),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Create a team",
+        description: `I need some people to help me maintain the app.`,
+        target_date: new Date(2021, 8, 15),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Launch a demo",
+        description: ``,
+        target_date: new Date(2021, 9, 4),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
+        story_id: 2,
+        parent_destination_id: null,
+        title: "Get some feedback from friends as users to test out features",
+        description: `Max has some experience with app testing so I should probably ask him.`,
+        target_date: new Date(2021, 10, 1),
+        accomplished: false,
+        importance: 8,
+        createdAt: new Date(2020, 12, 10),
+        updatedAt: new Date(2020, 12, 10)
+      },
+      {
         story_id: 3,
         parent_destination_id: null,
         title: "Get an inquiry to develop full-stack application for a company",
