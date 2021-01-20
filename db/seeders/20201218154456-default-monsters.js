16bcaabe1b1012cb83c9f928584efbf8d6c56c91
Yuka <yukamoribe@outlook.com>
2021-01-19 20:13:26 -0500

    Add seed data for monsters on story 2 and 3.

--- db/seeders/20201218154456-default-monsters.js
+++ db/seeders/20201218154456-default-monsters.js
@@ -39,6 +39,78 @@ module.exports = {
         createdAt: new Date(2020, 11, 5),
         updatedAt: new Date(2020, 11, 5)
       },
+      {
+        story_id: 2,
+        name: 'Sleeping In',
+        strength: 8,
+        image_id: 7,
+        times_defeated: 4,
+        createdAt: new Date(2020, 11, 5),
+        updatedAt: new Date(2020, 11, 5)
+      },
+      {
+        story_id: 2,
+        name: 'Late Night Gaming with Friends',
+        strength: 3,
+        image_id: 2,
+        times_defeated: 11,
+        createdAt: new Date(2020, 11, 6),
+        updatedAt: new Date(2020, 11, 6)
+      },
+      {
+        story_id: 2,
+        name: 'House Obligations',
+        strength: 3,
+        image_id: 3,
+        times_defeated: 9,
+        createdAt: new Date(2020, 11, 5),
+        updatedAt: new Date(2020, 11, 5)
+      },
+      {
+        story_id: 2,
+        name: 'Spending on Unnecessary Goods',
+        strength: 9,
+        image_id: 7,
+        times_defeated: 2,
+        createdAt: new Date(2020, 11, 5),
+        updatedAt: new Date(2020, 11, 5)
+      },
+      {
+        story_id: 3,
+        name: 'Concentration Loss',
+        strength: 10,
+        image_id: 8,
+        times_defeated: 4,
+        createdAt: new Date(2020, 11, 5),
+        updatedAt: new Date(2020, 11, 5)
+      },
+      {
+        story_id: 3,
+        name: 'Procastination',
+        strength: 4,
+        image_id: 4,
+        times_defeated: 20,
+        createdAt: new Date(2020, 11, 6),
+        updatedAt: new Date(2020, 11, 6)
+      },
+      {
+        story_id: 3,
+        name: 'Motivation Loss',
+        strength: 3,
+        image_id: 3,
+        times_defeated: 9,
+        createdAt: new Date(2020, 11, 5),
+        updatedAt: new Date(2020, 11, 5)
+      },
+      {
+        story_id: 3,
+        name: 'Duties',
+        strength: 2,
+        image_id: 1,
+        times_defeated: 10,
+        createdAt: new Date(2020, 11, 5),
+        updatedAt: new Date(2020, 11, 5)
+      },
     ], {});
   },
 
