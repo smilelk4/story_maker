b047ee6436769fc37a7f558a08371b05c2be2fb8
Yuka <yukamoribe@outlook.com>
2021-01-23 09:25:59 -0500

    Implement edit/delete icons to monsters and set view when editing monster.

--- client/src/components/story/Monster.js
+++ client/src/components/story/Monster.js
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloseIcon from '@material-ui/icons/Close';
 
-const Monster = ({...props}) => {
-  const { id, name, strength, image, timesDefeated, createdAt } = props;
+const Monster = ({ id, name, strength, image, timesDefeated, createdAt }) => {
+  const [viewMode, setViewMode] = useState('default');
+  const [newName, setNewName] = useState(name);
+  const [newStrength, setNewStrength] = useState(strength);
+
+  const handleEdit = async e => {
+    e.preventDefault();
+    // const data = editDestination({id, newTitle, newDescription, newTargetDate});
+    // if (!data.errors) {
+    //   setViewMode('default');
+    // }
+  }
+
+  const handleDelete = async e => {
+    e.preventDefault();
+    // const data = deleteDestination(id);
+    // if (!data.errors) {
+      // setViewMode('default');
+    // }
+  }
 
   return ( 
     <div className="monster">
-      <div className="monster__info">
-        <p className="mystory__title title">{name}</p>
-        <p className="strength">Strength: {strength}</p>
-        <p className="times-defeated">Times Defeated: {timesDefeated}</p>
-        <p className="created-at">Battling Since: {moment(createdAt).format("MM-DD-YYYY")}</p>
-      </div>
-      <img src={image} 
-             alt={id}/>
+      <span className="destination__menu">
+        {viewMode === 'default' ? (
+          <>
+            <EditIcon onClick={() => setViewMode('edit')}/>
+            <DeleteIcon onClick={() => setViewMode('delete')}/>
+          </>
+        ) : (
+          <p onClick={() => {setViewMode('default')}}><CloseIcon /></p>
+        )}
+      </span>
+      {viewMode === 'edit' && (
+        <form className='form--edit' onSubmit={handleEdit}>
+          <span>Name: </span>
+          <input 
+            type="text"
+            value={newName}
+            onChange={e => setNewName(e.target.value)}/>
+          <span>Monster Level: </span>
+          <input type="range" 
+                value={newStrength} 
+                min="1" 
+                max="10" 
+                name="strength"
+                step="1"
+                className="small"
+              onChange={e => setNewStrength(e.target.value)} />
+          <button type="submit">Save Change</button>
+        </form>
+      )}
+      {viewMode === 'default' && (
+        <>
+          <div className="monster__info">
+            <p className="mystory__title title">{name}</p>
+            <p className="strength">Strength: {strength}</p>
+            <p className="times-defeated">Times Defeated: {timesDefeated}</p>
+            <p className="created-at">Battling Since: {moment(createdAt).format("MM-DD-YYYY")}</p>
+          </div>
+          <img src={image} alt={id}/>
+        </>
+      )}
     </div>
   );
 }
